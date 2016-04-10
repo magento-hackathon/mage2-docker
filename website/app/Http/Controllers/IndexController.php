20c9b61675bf3c1363efb26c1e22b83aaf262047
Joseph Leedy <joseph@imaginemage.com>
2016-04-09 17:03:49 -0700

    Create Index page

---
+++ website/app/Http/Controllers/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    public function index(Request $request)
+    {
+        return view('index');
+    }
+}
