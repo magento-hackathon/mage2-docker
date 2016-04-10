d6a032c3f1d472ed2f8198c4f091309130b44a07
Joseph Leedy <joseph@imaginemage.com>
2016-04-09 17:26:06 -0700

    Install Laravel Elixir and Twitter Bootstrap

---
+++ website/gulpfile.js
@@ -0,0 +1,22 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(function(mix) {
+    var bootstrapPath = 'node_modules/bootstrap-sass/assets',
+        jQueryPath = 'node_modules/jquery/dist';
+
+    mix.sass('app.scss')
+        .copy(bootstrapPath + '/fonts', 'public/fonts')
+        .copy(bootstrapPath + '/javascripts/bootstrap.min.js', 'public/js')
+        .copy(jQueryPath + '/jquery.min.*', 'public/js');
+});
